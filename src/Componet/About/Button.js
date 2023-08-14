6c29697fdc14181cbd24841bf457a4e1cbad1423
Masoumeh Rezai <you@example.com>
2023-08-15 03:37:00 +0430

    add button

---
+++ src/Componet/About/Button.js
@@ -0,0 +1,8 @@
+const Button = (props) => {
+    return (
+        <button className='btn'>{props.childern}
+            Read more
+        </button>
+    )
+}
+export default Button;
--- src/Componet/About/Text.js
+++ src/Componet/About/Text.js
@@ -1,15 +1,22 @@
-import img from '../images/black-red.png'
+import img from '../images/black-red.png';
+import classes from './Text.module.css'
+import Button from './Button';
 const Text = (props) => {
     return (
-        <div>
-            <div>
+        <div className={classes.content}>
+            <div className={classes.text}>
                 <h1>Style is important</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br> sed do eiusmod tempor incididunt ut labore et dolore<br></br>
                     magna aliqua.
                  Ut enim ad minim veniam, quis nostrud<br></br> exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div>
+                    <Button> red</Button>
+
+                </div>
+
             </div>
             <div>
-                <img src={img} alt="" />
+                <img src={img} alt="" className={classes.img} />
             </div>
         </div>
     )
