ab967dfe07f293045f02e7e8138649fe6726abb5
Masoumeh Rezai <you@example.com>
2023-08-17 08:50:08 +0430

    add form input section

--- src/Componet/Form/Form.js
+++ src/Componet/Form/Form.js
@@ -1,15 +1,22 @@
 import { Fragment } from "react";
+import classes from './Form.module.css'
 
 const Form = (props) => {
     return (
         <Fragment>
-            <form>
-                <div>
+            <form >
+                <div className={classes.content}>
                     <input placeholder="Name" />
                     <input placeholder="Phone Number" />
+                </div>
+                <div className={classes.content} >
                     <input placeholder="Email" />
                     <input placeholder="Address" />
                 </div>
+                <div className={classes.content}>
+                    <input placeholder="Massage" className={classes.inp} />
+                </div>
+
             </form>
 
         </Fragment>
